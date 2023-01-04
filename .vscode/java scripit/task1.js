c4d53b80a0d54d7a11b46ab2db861e26c0e56cd8
SURYAIDURI <tejs4100@gmail.com>
2023-01-04 14:46:07 +0530

    Dynamic Styling

---
+++ .vscode/java scripit/task1.js
@@ -0,0 +1,29 @@
+// Implement the class User as per the youtuber. Every time a new user registers, keep a count of number of registered users.
+//At any given point of time if we do User.registeredUsers , i should be able to get the value .
+
+class User{
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password= password;
+
+    }
+    static countUsers(){
+        console.log('There are 50 users');
+    }
+    register(){
+        console.log(this.username+ ' is now registered');
+    }
+}
+    class Member extends User{
+        constructor(username, email, password, memberPackage){
+            super(username, email, password);
+            this.package = memberPackage;
+        }
+
+        getPackage(){
+            console.log(this.username +' is selected for the ' + this.package + 'package');
+        }
+    }
+    let surya = new Member('surya', 'surya@gmail.com', 134898, 'standard');
+    surya.register();
